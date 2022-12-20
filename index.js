b33620ea6930f1f1e1cb11a53f12d95874d70b97
tatylima <tatianagandra78@gmail.com>
2022-12-20 18:49:02 -0300

    feat: modification of project

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const express = require("express");
+const path = require("path");
+
 const app = express();
 const port = 3000;
 
 app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
 app.get("/index", (req, res) => {
-  res.render("../views/index");
+  res.render("index");
 });
 
 app.listen(port, () =>
