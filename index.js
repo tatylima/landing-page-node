ac26db5be317e3a2e1bc404cbfeda905152c1b40
tatylima <tatianagandra78@gmail.com>
2022-12-20 18:38:23 -0300

    feat: renderizando ejs

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-// Importa o módulo express para esse arquivo
 const express = require("express");
-// Instancia uma referência do express no projeto
 const app = express();
-const port = 3000; // Const para armanezar a porta do servidor
+const port = 3000;
 
-// Rota principal que recebe uma função de callback que recebe dois parametros: 
-// req de requisição
-// res de resposta
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-    res.send("Hello World"); // Responde a requisição da rota com um texto.
-  });
+  res.send("Hello World");
+});
 
-// Adicionando a const port e uma arow function de callback para mostrar no console que o servidor está rodando.
-app.listen(port, () => console.log(`Servidor rodando em http://localhost:${port}`));
+app.get("/index", (req, res) => {
+  res.render("../views/index");
+});
+
+app.listen(port, () =>
+  console.log(`Servidor rodando em http://localhost:${port}`)
+);
