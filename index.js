5d29ee62c61267b97bc0d939251347571ac3281e
tatylima <tatianagandra78@gmail.com>
2022-12-20 18:57:06 -0300

    feat: create script.js

--- index.js
+++ index.js
@@ -6,13 +6,21 @@ const port = 3000;
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded());
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  const devList = ["Backend", "Frontend", "Fullstack"];
+  const analyticsList = ["Engenharia de dados", "Ciência de dados"];
+  res.render("index", {
+    titulo: "Blue",
+    devList: devList,
+    analyticsList: analyticsList,
+  });
 });
 
-app.get("/index", (req, res) => {
-  res.render("index");
+app.post("/subscription", (req, res) => {
+  const { nome, email } = req.body;
+  res.send({nome: nome, email: email});
 });
 
 app.listen(port, () =>
